f060545e0f5a652b393974b525d1710aa1df9cf2
pjs <qodlsrud20712@gmail.com>
2020-06-20 18:39:13 +0900

    초기 커밋.

---
+++ Csharp_Study/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCsharp
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#");
+            Console.WriteLine("Hello " + args[0]);
+
+            Console.ReadKey();
+        }
+    }
+}
