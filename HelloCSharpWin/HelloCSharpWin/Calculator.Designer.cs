936509a5af9ff2a613738c9a8fb87db5017dd2ac
pjs <qodlsrud20712@gmail.com>
2020-07-05 01:58:06 +0900

    sub,mul,div 추가 및 수정

--- HelloCSharpWin/HelloCSharpWin/Calculator.Designer.cs
+++ HelloCSharpWin/HelloCSharpWin/Calculator.Designer.cs
@@ -34,6 +34,17 @@
             this.numplus = new System.Windows.Forms.Button();
             this.Operation = new System.Windows.Forms.Label();
             this.result = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.num7 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.num0 = new System.Windows.Forms.Button();
+            this.numdiv = new System.Windows.Forms.Button();
+            this.nummul = new System.Windows.Forms.Button();
+            this.numsub = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // num1
@@ -89,7 +100,7 @@
             // 
             // result
             // 
-            this.result.Location = new System.Drawing.Point(391, 196);
+            this.result.Location = new System.Drawing.Point(381, 333);
             this.result.Name = "result";
             this.result.Size = new System.Drawing.Size(75, 48);
             this.result.TabIndex = 5;
@@ -97,11 +108,132 @@
             this.result.UseVisualStyleBackColor = true;
             this.result.Click += new System.EventHandler(this.result_Click);
             // 
+            // num3
+            // 
+            this.num3.Location = new System.Drawing.Point(217, 124);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(62, 53);
+            this.num3.TabIndex = 6;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = true;
+            this.num3.Click += new System.EventHandler(this.num3_Click);
+            // 
+            // num4
+            // 
+            this.num4.Location = new System.Drawing.Point(45, 194);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(62, 53);
+            this.num4.TabIndex = 7;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = true;
+            this.num4.Click += new System.EventHandler(this.num4_Click);
+            // 
+            // num5
+            // 
+            this.num5.Location = new System.Drawing.Point(129, 194);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(62, 53);
+            this.num5.TabIndex = 8;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = true;
+            this.num5.Click += new System.EventHandler(this.num5_Click);
+            // 
+            // num6
+            // 
+            this.num6.Location = new System.Drawing.Point(217, 196);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(62, 53);
+            this.num6.TabIndex = 9;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = true;
+            this.num6.Click += new System.EventHandler(this.num6_Click);
+            // 
+            // num7
+            // 
+            this.num7.Location = new System.Drawing.Point(45, 263);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(62, 53);
+            this.num7.TabIndex = 10;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = true;
+            this.num7.Click += new System.EventHandler(this.num7_Click);
+            // 
+            // num8
+            // 
+            this.num8.Location = new System.Drawing.Point(129, 263);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(62, 53);
+            this.num8.TabIndex = 11;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = true;
+            this.num8.Click += new System.EventHandler(this.num8_Click);
+            // 
+            // num9
+            // 
+            this.num9.Location = new System.Drawing.Point(217, 263);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(62, 53);
+            this.num9.TabIndex = 12;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = true;
+            this.num9.Click += new System.EventHandler(this.num9_Click);
+            // 
+            // num0
+            // 
+            this.num0.Location = new System.Drawing.Point(128, 333);
+            this.num0.Name = "num0";
+            this.num0.Size = new System.Drawing.Size(62, 53);
+            this.num0.TabIndex = 13;
+            this.num0.Text = "0";
+            this.num0.UseVisualStyleBackColor = true;
+            this.num0.Click += new System.EventHandler(this.num0_Click);
+            // 
+            // numdiv
+            // 
+            this.numdiv.Location = new System.Drawing.Point(493, 196);
+            this.numdiv.Name = "numdiv";
+            this.numdiv.Size = new System.Drawing.Size(75, 53);
+            this.numdiv.TabIndex = 14;
+            this.numdiv.Text = "/";
+            this.numdiv.UseVisualStyleBackColor = true;
+            this.numdiv.Click += new System.EventHandler(this.numdiv_Click);
+            // 
+            // nummul
+            // 
+            this.nummul.Location = new System.Drawing.Point(391, 196);
+            this.nummul.Name = "nummul";
+            this.nummul.Size = new System.Drawing.Size(75, 53);
+            this.nummul.TabIndex = 15;
+            this.nummul.Text = "*";
+            this.nummul.UseVisualStyleBackColor = true;
+            this.nummul.Click += new System.EventHandler(this.nummul_Click);
+            // 
+            // numsub
+            // 
+            this.numsub.Location = new System.Drawing.Point(493, 124);
+            this.numsub.Name = "numsub";
+            this.numsub.Size = new System.Drawing.Size(75, 53);
+            this.numsub.TabIndex = 16;
+            this.numsub.Text = "-";
+            this.numsub.UseVisualStyleBackColor = true;
+            this.numsub.Click += new System.EventHandler(this.numsub_Click);
+            // 
             // Calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numsub);
+            this.Controls.Add(this.nummul);
+            this.Controls.Add(this.numdiv);
+            this.Controls.Add(this.num0);
+            this.Controls.Add(this.num9);
+            this.Controls.Add(this.num8);
+            this.Controls.Add(this.num7);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num3);
             this.Controls.Add(this.result);
             this.Controls.Add(this.Operation);
             this.Controls.Add(this.numplus);
@@ -122,6 +254,17 @@
         private System.Windows.Forms.Button numplus;
         private System.Windows.Forms.Label Operation;
         private System.Windows.Forms.Button result;
+        private System.Windows.Forms.Button num3;
+        private System.Windows.Forms.Button num4;
+        private System.Windows.Forms.Button num5;
+        private System.Windows.Forms.Button num6;
+        private System.Windows.Forms.Button num7;
+        private System.Windows.Forms.Button num8;
+        private System.Windows.Forms.Button num9;
+        private System.Windows.Forms.Button num0;
+        private System.Windows.Forms.Button numdiv;
+        private System.Windows.Forms.Button nummul;
+        private System.Windows.Forms.Button numsub;
     }
 }
 
--- HelloCSharpWin/HelloCSharpWin/Calculator.cs
+++ HelloCSharpWin/HelloCSharpWin/Calculator.cs
@@ -30,11 +30,23 @@ namespace HelloCSharpWin
             return sum;
         }
 
-        public int sub(int num1, int num2)
+        public int Sub(int num1, int num2)
         {
             int sum = num1 - num2;
             return sum;
         }
+        public int Mul(int num1, int num2)
+        {
+            int sum = num1 * num2;
+            return sum;
+        }
+
+        public int Div(int num1, int num2)
+        {
+            int sum = num1/num2;
+            return sum;
+        }
+
 
         private void num1_Click(object sender, EventArgs e)
         {
@@ -64,8 +76,15 @@ namespace HelloCSharpWin
         }
 
         private void numplus_Click(object sender, EventArgs e)
-        {
-            Operation.Text = Operation.Text + "+";
+        {   
+            if(Operation.Text != "")
+            {
+                Operation.Text = Operation.Text + "+";
+            }
+            else
+            {
+                Operation.Text = numscreen.Text + "+";
+            }
         }
 
         private void result_Click(object sender, EventArgs e)
@@ -74,14 +93,130 @@ namespace HelloCSharpWin
             int num2 = 0;
             string OperText;
             OperText = Operation.Text;
-            //OperText.Replace("+-*/", "");
-            string[] res = OperText.Split('+');
-            int.TryParse(res[0], out num1);
-            int.TryParse(res[1], out num2);
-            int sum = Add(num1, num2);
-            Operation.Text = "";
-            numscreen.Text = sum.ToString();
+            string plus = "+";
+            string minus = "-";
+            string mul = "*";
+            string div = "/";
+            if(OperText.Contains(plus))
+            {
+                string[] res = OperText.Split('+');
+                int.TryParse(res[0], out num1);
+                int.TryParse(res[1], out num2);
+                int sum = Add(num1, num2);
+                numscreen.Text = sum.ToString();
+            }
+            else if (OperText.Contains(minus))
+            {
+                string[] res = OperText.Split('-');
+                int.TryParse(res[0], out num1);
+                int.TryParse(res[1], out num2);
+                int sum = Sub(num1, num2);
+                numscreen.Text = sum.ToString();
+            }
+            else if (OperText.Contains(mul))
+            {
+                string[] res = OperText.Split('*');
+                int.TryParse(res[0], out num1);
+                int.TryParse(res[1], out num2);
+                int sum = Mul(num1, num2);
+                numscreen.Text = sum.ToString();
+            }
+            else if (OperText.Contains(div))
+            {
+                string[] res = OperText.Split('/');
+                int.TryParse(res[0], out num1);
+                int.TryParse(res[1], out num2);
+                int sum = Div(num1, num2);
+                numscreen.Text = sum.ToString();
+            }
 
+            //OperText.Replace("+-*/", "");
+            Operation.Text = "";
+        }
+
+        private void num3_Click(object sender, EventArgs e)
+        {
+            numscreen.Text = "";
+            SetNum("3");
+        }
+
+        private void num4_Click(object sender, EventArgs e)
+        {
+            numscreen.Text = "";
+            SetNum("4");
+        }
+
+        private void num5_Click(object sender, EventArgs e)
+        {
+            numscreen.Text = "";
+            SetNum("5");
+        }
+
+        private void num6_Click(object sender, EventArgs e)
+        {
+            numscreen.Text = "";
+            SetNum("6");
+        }
+
+        private void num7_Click(object sender, EventArgs e)
+        {
+            numscreen.Text = "";
+            SetNum("7");
+        }
+
+        private void num8_Click(object sender, EventArgs e)
+        {
+            numscreen.Text = "";
+            SetNum("8");
+        }
+
+        private void num9_Click(object sender, EventArgs e)
+        {
+            numscreen.Text = "";
+            SetNum("9");
+        }
+
+        private void num0_Click(object sender, EventArgs e)
+        {
+            numscreen.Text = "";
+            SetNum("0");
+        }
+
+        private void numsub_Click(object sender, EventArgs e)
+        {
+            if (Operation.Text != "")
+            {
+                Operation.Text = Operation.Text + "-";
+            }
+            else
+            {
+                Operation.Text = numscreen.Text + "-";
+            }
+
+        }
+
+        private void nummul_Click(object sender, EventArgs e)
+        {
+            if (Operation.Text != "")
+            {
+                Operation.Text = Operation.Text + "*";
+            }
+            else
+            {
+                Operation.Text = numscreen.Text + "*";
+            }
+        }
+
+        private void numdiv_Click(object sender, EventArgs e)
+        {
+            if (Operation.Text != "")
+            {
+                Operation.Text = Operation.Text + "/";
+            }
+            else
+            {
+                Operation.Text = numscreen.Text + "/";
+            }
         }
     }
 }
